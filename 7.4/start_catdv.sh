84c45083a1c0a5c1aa72266c28d67eb056477c84
root <root@filmtar.termeszetfilm.hu>
2019-04-14 19:40:53 +0200

    Patched 7.4.3 version added

--- 7.4/start_catdv.sh
+++ 7.4/start_catdv.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-docker run -d -v $PWD/catdv.properties:/usr/local/catdvServer/catdv.properties -p 192.168.1.251:1099:1099 --name catdvserver7.4 catdvserver:7.4.2 "$@"
-addhost.sh catdvserver7.4
+NAME=catdvserver7.4
+docker rm $NAME
+docker run -d --link docker.mysql.service:mysql -v $PWD/catdv.properties:/usr/local/catdvServer/catdv.properties -p 192.168.1.251:1099:1099 -p 192.168.1.251:8080:8080 --name $NAME catdvserver:7.4.3 "$@"
+addhost.sh $NAME
 #docker run --net=host -v $PWD/catdv.properties:/usr/local/catdvServer/catdv.properties --name catdvserver catdvserver:7.3.5p10 "$@"
